84dd4b91ea64f9ddffe43e997a2a92e37946f565
FridaPolished <fridapulido@gmail.com>
2019-12-10 22:11:53 -0800

    Fix tooltip on book cover and button to add shelves also fixed positioning of footer

--- frontend/components/book/book_index_item.jsx
+++ frontend/components/book/book_index_item.jsx
@@ -3,29 +3,44 @@ import {Link} from 'react-router-dom';
 import BookshelfAddContainer from '../bookshelf/bookshelf_add_container';
 import ReactTooltip from "react-tooltip";
 
-const BookIndexItem = ({ book }) => (
-  <div>
-    <Link to={`/books/${book.id}`}>
-      <img
-        className="book-cover-image"
-        src={book.coverUrl}
-        alt={book.title}
-        data-tip
-        data-for={`${book.id}`}
-      />
-    <ReactTooltip id={`${book.id}`} className='custom' effect="solid" place="right">
-      <h2>{`${book.title}`}</h2>
-      <br />
-      <span>by {`${book.author}`}</span>
-      <br />
-      <p>{`${book.description}`}</p>
-      <br />
-    </ReactTooltip>
-    </Link>
-    <div id="book-show">
-      <BookshelfAddContainer book={book} />
+const BookIndexItem = ({ book }) => {
+	let sampleDescription = book.description.substr(0,300);
+
+	return (
+    <div>
+      <Link to={`/books/${book.id}`}>
+        <img
+          className="book-cover-image"
+          src={book.coverUrl}
+          alt={book.title}
+          data-tip
+          data-for={`${book.id}`}
+        />
+        <ReactTooltip
+          id={`${book.id}`}
+          className="custom"
+          effect="solid"
+          place="right"
+        >
+          <h2 className="b-title">{`${book.title}`}</h2>
+          <br />
+          <span>by {`${book.author}`}</span>
+          <br />
+          <br />
+          <span>
+            <p className="s-paragraph">{`${sampleDescription}`}...</p>
+          </span>
+          <br />
+          <br />
+          <span className="continue">
+            ...click on cover to continue reading
+          </span>
+        </ReactTooltip>
+      </Link>
+      <div id="book-show">
+        <BookshelfAddContainer book={book} />
+      </div>
     </div>
-  </div>
-);
+  );};
 
 export default BookIndexItem;
